8484696a2b515bbd25a0c0017b8456296db37f0f
“Ibrahim <ibsaad@noon.com>
2024-10-04 20:04:51 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
         <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
