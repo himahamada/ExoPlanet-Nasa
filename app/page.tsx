fd08e230184d08f8f5cde72b7c18c90238687c56
“Ibrahim <ibsaad@noon.com>
2024-10-05 00:40:25 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,82 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+import { Metadata } from 'next';
 
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import AnimatedWrapper from '_core/components/AnimatedWrapper';
+import HomeHeader from '_core/components/HomeHeader';
+// import { getHomePageData } from '@/utils/homepage';
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+// import TileList from '../_core/components/TileList';
+
+// import Search from '../_core/components/Search';
+// eslint-disable-next-line css-modules/no-unused-class
+import styles from './page.module.scss';
+import Tile from '_core/components/Tile';
+
+
+type TPageData = {
+  title: string;
+  desc: string;
+};
+
+function Description(desc: string) {
+
+  const formattedDesc = desc.split('\n').map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+      <br />
+    </span>
+  ));
+
+  return <div>{formattedDesc}</div>;
 }
+
+
+
+
+const HomePage = async () => {
+    // const homePageData = await getHomePageData();
+    const ExploreData: TPageData = {
+        title: 'Explore Exovision',
+        desc: "Welcome to Exovision, your launchpad to an epic adventure across the stars! Get ready to blast off into the unknown and explore the far reaches of the universe in this thrilling educational game. Strap in, future astronauts! now's your chance to dive into the world of space exploration. Discover mysterious planets, navigate the galaxy, and unlock the secrets of NASA’s most cutting-edge missions—all through immersive, interactive gameplay! Are you ready to embark on a journey like no other? The universe is waiting!",
+      };
+
+      const AboutData: TPageData = {
+        title: 'About',
+        desc: Description("Exploring the Universe with NASA Exovision was born out of the excitement and innovation of the NASA Space Apps Challenge, a global hackathon that brings together bright minds to tackle real-world space challenges. Inspired by NASA’s ongoing missions and the boundless mysteries of the cosmos, our team set out to create an interactive, educational game that puts space exploration at your fingertips. \n In Exovision, you’re not just exploring familiar planets within our solar system—you’re venturing far beyond, to distant worlds that NASA and astronomers have only recently begun to uncover. Get ready to chart your course through some of the most exciting exoplanets discovered to date. Here are just a few of the fascinating destinations waiting for you: \n 51 Pegasi b: The First Discovered Exoplanet \nIn 1995, astronomers made a groundbreaking discovery: 51 Pegasi b, the first planet ever found orbiting a sun-like star outside our solar system. Known as a “hot Jupiter,” this gas giant orbits extremely close to its star, making it scorchingly hot and wildly different from any planets in our system. While it might not be habitable, its discovery opened the door to finding countless other exoplanets. \nEpsilon Eridani b: A Neighboring Gas Giant \nOne of the closest known exoplanets, Epsilon Eridani b is a gas giant located just 10.5 light-years away in the constellation Eridanus. Discovered in 2000, it orbits its star in a path similar to Jupiter’s, making it a fascinating target for scientists looking for solar systems like ours. It’s surrounded by a dust disk, hinting at the possibility of even more planets within its system. \nIn Exovision, you’ll visit 51 Pegasi b and Epsilon Eridani band and learn about discovering how astronomers study nearby exoplanets and the potential for more hidden worlds in our galactic neighborhood. \n Chart Your Own Course \nThese are just a few of the destinations waiting for you in the game. As you continue your journey, you’ll uncover more distant exoplanets, each with its own mysteries and wonders. From scorching gas giants to potentially habitable super-Earths, the universe is full of worlds to explore—and it’s all within your reach. \nReady to start your interstellar journey? The stars are waiting for you."),
+      };
+
+
+      
+    return (
+        <AnimatedWrapper>
+            <div className={styles.container}>
+                {/* <HomeHeader /> */}
+                {/* <Search /> */}
+                {/* <TileList homePageData={homePageData} /> */}
+                <div className={styles.tileContainer}>
+                    <Tile data={ExploreData} />
+
+                    <Tile data={AboutData} />
+
+                    {/* <Tile data={ExploreData} />
+
+                    <Tile data={ExploreData} />
+
+                    <Tile data={ExploreData} /> */}
+
+                </div>
+            </div>
+        </AnimatedWrapper>
+    );
+};
+export const generateMetadata = async () => {
+    const metadata: Metadata = {};
+    metadata.title = 'Home';
+    metadata.description = 'Lighthouse homepage with links to various pages.';
+
+    return metadata;
+};
+
+export default HomePage;
